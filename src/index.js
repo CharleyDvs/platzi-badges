2b1bb668935cf52ff3ed281bf938d7c528e14938
CharleyDvs <ing.carlosjaime@outlook.com>
2020-12-15 22:25:41 -0600

    Render app class

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+// const element = document.createElement('h1');
+// element.innerText = 'Hello, Platzi Badges';
+
+// const container = document.getElementById('app');
+
+// container.appendChild(element);
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const element = <h1>Hello, Platzi Badges from React</h1>
 const container = document.getElementById('app');
 
-const element = <h1>Hello, Platzi Badges</h1>;
 ReactDOM.render(element, container);
