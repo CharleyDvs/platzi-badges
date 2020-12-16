7852ac37b8feba9e3703d41967851132cab53480
CharleyDvs <ing.carlosjaime@outlook.com>
2020-12-15 22:45:17 -0600

    Playing with jsx

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges';
-
-// const container = document.getElementById('app');
-
-// container.appendChild(element);
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges from React</h1>
+// const element = <h1>Hello, Platzi Badges from React</h1>
+const name = 'Richard';
+const element = React.createElement('h1', {}, `Hello ${name}`);
 const container = document.getElementById('app');
+const jsx = 
+    <div>
+        <h1>Hello</h1>
+        <p>I'm an engineer</p>
+    </div>
 
-ReactDOM.render(element, container);
+ReactDOM.render(jsx, container);
