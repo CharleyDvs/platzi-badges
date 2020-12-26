5114a214057bebf9f8136ed7429251e4788e14b3
CharleyDvs <ing.carlosjaime@outlook.com>
2020-12-25 20:18:09 -0600

    Added props to the badge component

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -5,24 +5,21 @@ import confLogo from "../images/badge-header.svg";
 
 class Badge extends React.Component {
   render() {
+    const { firstName, lastName, avatar, jobTitle, twitter } = this.props;
     return (
       <div className="badge">
         <div className="badge_header">
           <img src={confLogo} alt="logo de la conferencia" />
         </div>
         <div className="badge_section-name">
-          <img
-            className="badge_avatar"
-            src="https://www.gravatar.com/avatar?d=identicon"
-            alt="avatar"
-          />
+          <img className="badge_avatar" src={avatar} alt="avatar" />
           <h1>
-            Richard <br /> Kaufman
+            {firstName} <br /> {lastName}
           </h1>
         </div>
         <div className="badge_section-info">
-          <h3>Frontend Engineer</h3>
-          <span>@sparragus</span>
+          <h3>{jobTitle}</h3>
+          <span>{twitter}</span>
         </div>
         <div className="badge_footer">#platziconf</div>
       </div>
--- src/index.js
+++ src/index.js
@@ -9,4 +9,13 @@ import Badge from "./components/Badge";
 // const element = <h1>Hello, Platzi Badges from React</h1>
 const container = document.getElementById("app");
 
-ReactDOM.render(<Badge />, container);
+ReactDOM.render(
+  <Badge
+    firstName="Richard"
+    lastName="Kaufman"
+    avatar="https://www.gravatar.com/avatar?d=identicon"
+    jobTitle="Frontend Engineer"
+    twitter="@sparragus"
+  />,
+  container
+);
