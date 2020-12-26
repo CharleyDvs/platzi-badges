c65fc6e59e158138566aaa8bb99b6211f54ca5e7
CharleyDvs <ing.carlosjaime@outlook.com>
2020-12-25 19:51:14 -0600

    Added badge component

---
+++ src/components/Badge.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import confLogo from "../images/badge-header.svg";
+
+class Badge extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <img src={confLogo} alt="logo de la conferencia" />
+        </div>
+        <div>
+          <img src="" alt="avatar" />
+          <h1>
+            Richard <br /> Kaufman
+          </h1>
+        </div>
+        <div>
+          <p>Frontend Engineer</p>
+          <p>@sparragus</p>
+        </div>
+        <div>#platziconf</div>
+      </div>
+    );
+  }
+}
+
+export default Badge;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// const element = <h1>Hello, Platzi Badges from React</h1>
-const name = 'Richard';
-const element = React.createElement('h1', {}, `Hello ${name}`);
-const container = document.getElementById('app');
-const jsx = 
-    <div>
-        <h1>Hello</h1>
-        <p>I'm an engineer</p>
-    </div>
+import Badge from './components/Badge';
 
-ReactDOM.render(jsx, container);
+// const element = <h1>Hello, Platzi Badges from React</h1>
+const container = document.getElementById('app');
+
+ReactDOM.render(<Badge />, container);
