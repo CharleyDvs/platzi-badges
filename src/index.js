bf55231166f07c91a022bfc1dcbe4a5866109a50
CharleyDvs <ing.carlosjaime@outlook.com>
2020-12-25 20:44:48 -0600

    Added new badge page

--- src/index.js
+++ src/index.js
@@ -4,18 +4,9 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "./global.css";
 
-import Badge from "./components/Badge";
+import BadgeNew from "./pages/BadgeNew";
 
 // const element = <h1>Hello, Platzi Badges from React</h1>
 const container = document.getElementById("app");
 
-ReactDOM.render(
-  <Badge
-    firstName="Richard"
-    lastName="Kaufman"
-    avatar="https://www.gravatar.com/avatar?d=identicon"
-    jobTitle="Frontend Engineer"
-    twitter="@sparragus"
-  />,
-  container
-);
+ReactDOM.render(<BadgeNew />, container);
